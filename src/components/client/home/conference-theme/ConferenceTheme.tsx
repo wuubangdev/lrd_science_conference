438c25f2e342aaa402ca24fe516004e5f70fafe5
wuubangdev <wuubangdev@gmail.com>
2025-04-14 18:54:52 +0700

    up intro

--- src/components/client/home/conference-theme/ConferenceTheme.tsx
+++ src/components/client/home/conference-theme/ConferenceTheme.tsx
@@ -1,3 +1,4 @@
+import { Image } from 'antd'
 import React from 'react'
 
 const listTheme: string[] = [
@@ -15,20 +16,29 @@ const listTheme: string[] = [
 
 const ConferenceTheme: React.FC = () => {
     return (
-        <div className='w-full my-container py-20'>
-            <h1 className='text-4xl font-medium mb-8'>Chủ đề hội thảo</h1>
-            <div className='grid md:grid-cols-2 grid-cols-1 gap-6'>
+        <div className='w-full my-container flex flex-col items-center py-20 gap-4'>
+            <div className='text-4xl font-medium mb-8 gradient-text'>Chủ đề hội thảo</div>
+            <div className='grid md:grid-cols-2 grid-cols-1 gap-6 w-full'>
                 <div>
                     <ul>
                         {listTheme && listTheme.map((item, i) => {
                             return (
-                                <li key={i}>{item}</li>
+                                <li key={i} className='flex gap-2 py-1 text-lg cursor-pointer hover:text-black/70 hover:bg-blue-300 px-2 rounded-lg hover:translate-x-1 transition-all duration-200 ease-in-out'>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M4.098 19.902a3.75 3.75 0 0 0 5.304 0l6.401-6.402M6.75 21A3.75 3.75 0 0 1 3 17.25V4.125C3 3.504 3.504 3 4.125 3h5.25c.621 0 1.125.504 1.125 1.125v4.072M6.75 21a3.75 3.75 0 0 0 3.75-3.75V8.197M6.75 21h13.125c.621 0 1.125-.504 1.125-1.125v-5.25c0-.621-.504-1.125-1.125-1.125h-4.072M10.5 8.197l2.88-2.88c.438-.439 1.15-.439 1.59 0l3.712 3.713c.44.44.44 1.152 0 1.59l-2.879 2.88M6.75 17.25h.008v.008H6.75v-.008Z" />
+                                    </svg>
+
+
+                                    {item}
+                                </li>
                             )
                         })}
                     </ul>
                 </div>
                 <div className=''>
-                    <div className='w-full aspect-[16/9] bg-amber-300 rounded-2xl'></div>
+                    <div className='w-full h-full rounded-lg relative overflow-hidden'>
+                        <Image src='/tnmt.jpg' alt='tnmt' className='object-cover rounded-lg' />
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/client/home/introduction/Introduction.tsx
+++ src/components/client/home/introduction/Introduction.tsx
@@ -7,11 +7,11 @@ const Introduction: React.FC = () => {
             <h1 className='text-4xl font-medium mb-10 text-center shadow-text'>Lời mời tham gia</h1>
             <div className='grid md:grid-cols-2 grid-cols-1 gap-8'>
                 <div className=''>
-                    <div className='w-full aspect-[16/9] bg-amber-300 rounded-lg relative overflow-hidden'>
-                        <Image src='/ctu.jpg' alt='ctu' className='object-cover' />
+                    <div className='w-full h-full rounded-lg relative overflow-hidden'>
+                        <Image src='/ctu.jpg' alt='ctu' className='object-cover rounded-lg' />
                     </div>
                 </div>
-                <div className='text-justify '>
+                <div className='text-justify text-lg text-gray-700 font-medium'>
                     Tiếp nối thành công rực rỡ của Hội thảo Quản lý Đất đai lần thứ hai tại Trường Đại học Nông Lâm, Đại học Huế, chúng tôi hân hạnh thông báo Hội thảo Quản lý Đất đai lần thứ ba với chủ đề "Quản lý đất đai bền vững" (SLM2025) sẽ diễn ra tại Đại học Cần Thơ. Hội thảo được đồng tổ chức bởi Trường Đại học Cần Thơ, Học viện Nông nghiệp Việt Nam và Trường Đại học Nông Lâm - Đại học Huế. Sự kiện này là diễn đàn lý tưởng để các nhà khoa học chia sẽ những kết quả nghiên cứu cơ bản và ứng dụng công nghệ, nhằm phục vụ cho quản lý và phát triển bền vững tài nguyên. Qua đó, hội thảo sẽ tạo cơ hội đề xuất các hướng nghiên cứu, hợp tác và phát triển trong tương lai. Đồng thời, đây cũng là diễn đàn cho học viên, nghiên cứu sinh, nơi các em có thể báo cáo và khám phá những kết quả mới mẻ trong lĩnh vực Quản lý đất đai.
                 </div>
             </div>
