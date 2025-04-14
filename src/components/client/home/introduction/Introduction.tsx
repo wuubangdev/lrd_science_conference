cd6bd522b9ab4d72e3e578837cefcd61c1307bf9
wuubangdev <wuubangdev@gmail.com>
2025-04-14 17:42:11 +0700

    up intro

--- src/components/client/home/introduction/Introduction.tsx
+++ src/components/client/home/introduction/Introduction.tsx
@@ -1,14 +1,17 @@
+import { Image } from 'antd'
 import React from 'react'
 
 const Introduction: React.FC = () => {
     return (
-        <div className='w-full my-container py-20 bg-blue-50'>
-            <h1 className='text-4xl font-medium mb-8'>Giới thiệu</h1>
-            <div className='grid md:grid-cols-2 grid-cols-1 gap-6'>
+        <div className='w-full my-container py-14 bg-blue-50'>
+            <h1 className='text-4xl font-medium mb-10 text-center shadow-text'>Lời mời tham gia</h1>
+            <div className='grid md:grid-cols-2 grid-cols-1 gap-8'>
                 <div className=''>
-                    <div className='w-full aspect-[16/9] bg-amber-300 rounded-2xl'></div>
+                    <div className='w-full aspect-[16/9] bg-amber-300 rounded-lg relative overflow-hidden'>
+                        <Image src='/ctu.jpg' alt='ctu' className='object-cover' />
+                    </div>
                 </div>
-                <div className='text-justify'>
+                <div className='text-justify '>
                     Tiếp nối thành công rực rỡ của Hội thảo Quản lý Đất đai lần thứ hai tại Trường Đại học Nông Lâm, Đại học Huế, chúng tôi hân hạnh thông báo Hội thảo Quản lý Đất đai lần thứ ba với chủ đề "Quản lý đất đai bền vững" (SLM2025) sẽ diễn ra tại Đại học Cần Thơ. Hội thảo được đồng tổ chức bởi Trường Đại học Cần Thơ, Học viện Nông nghiệp Việt Nam và Trường Đại học Nông Lâm - Đại học Huế. Sự kiện này là diễn đàn lý tưởng để các nhà khoa học chia sẽ những kết quả nghiên cứu cơ bản và ứng dụng công nghệ, nhằm phục vụ cho quản lý và phát triển bền vững tài nguyên. Qua đó, hội thảo sẽ tạo cơ hội đề xuất các hướng nghiên cứu, hợp tác và phát triển trong tương lai. Đồng thời, đây cũng là diễn đàn cho học viên, nghiên cứu sinh, nơi các em có thể báo cáo và khám phá những kết quả mới mẻ trong lĩnh vực Quản lý đất đai.
                 </div>
             </div>
@@ -16,4 +19,4 @@ const Introduction: React.FC = () => {
     )
 }
 
-export default Introduction
+export default Introduction;
