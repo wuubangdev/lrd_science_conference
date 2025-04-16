13f601e7a8950b453e51513b5091d1940bbe1620
wuubangdev <wuubangdev@gmail.com>
2025-04-16 21:40:50 +0700

    end: home

--- src/components/client/home/guideline/HomeGuideline.tsx
+++ src/components/client/home/guideline/HomeGuideline.tsx
@@ -1,13 +1,6 @@
 import { Tag } from 'antd';
 import React from 'react';
 
-const guideline: string[] = [
-    "Hình thức viết và cách thức nộp bài tóm tắt tham gia Hội thảo vui lòng xem thông tin hướng dẫn",
-    "Thông tin chi tiết xin vui lòng liên hệ Ban thư ký Hội thảo: TS. Phan Kiều Diễm, Khoa Môi trường và Tài nguyên Thiên nhiên, Trường Đại học Cần Thơ",
-    "E-mail: lrconference@ctu.edu.vn, hoặc pkdiem@ctu.edu.vn",
-    "Điện thoại: +84-983.954.243"
-]
-
 const HomeGuideline: React.FC = () => {
     return (
         <div className='w-full my-container py-20'>
